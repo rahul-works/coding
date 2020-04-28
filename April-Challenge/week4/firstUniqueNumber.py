81dab64ca975d5cd73d84432f716f94ceace2379
Rahul Thakur <rahul.thakur9985@gmail.com>
2020-04-28 15:52:41 +0530

    py soln

--- April-Challenge/week4/firstUniqueNumber.py
+++ April-Challenge/week4/firstUniqueNumber.py
@@ -1,20 +1,29 @@
 class FirstUnique(object):
 
-    def __init__(self, nums):
-        """
-        :type nums: List[int]
-        """
+    def __init__(self, nums: List[int]):
+        self.deque = collections.deque()
+        self.lookup = {}
+        
+        for num in nums:
+            self.add(num)
+
+    def showFirstUnique(self) -> int:
+        if len(self.deque) == 0:
+            return -1
+
+        while len(self.deque) > 0 and self.deque[0] in self.lookup and self.lookup[self.deque[0]] >= 2:
+            self.deque.popleft()
+
+        if len(self.deque) == 0:
+            return -1
+        return self.deque[0] 
         
 
-    def showFirstUnique(self):
-        """
-        :rtype: int
-        """
+    def add(self, value) -> None:
+        if value in self.lookup:
+            self.lookup[value] += 1
+        else:
+            self.lookup[value] = 1
         
-
-    def add(self, value):
-        """
-        :type value: int
-        :rtype: None
-        """
+        self.deque.append(value)
         
