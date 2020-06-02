8082d2a1d4e9d1f033fe215b1b30e775be477caf
Rahul Thakur <rahul.thakur9985@gmail.com>
2020-06-02 13:06:45 +0530

    invertTree

---
+++ June-Challenge/week1/invertedBinary.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val);
+    this.left = (left===undefined ? null : left);
+    this.right = (right===undefined ? null : right);
+}
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (root === null) {
+        return null;
+    }
+    let left = invertTree(root.left);
+    let right = invertTree(root.right);
+    root.left = right.val;
+    root.right = left.val;
+    return root;
+};
+
+console.log(invertTree([4,2,7,1,3,6,9]));
