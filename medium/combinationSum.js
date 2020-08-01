ca6a520f98de7e6a0647be9f8454358a94db5e14
Rahul Thakur <rahul.thakur9985@gmail.com>
2020-08-01 18:56:22 +0530

    combinationSum

---
+++ medium/combinationSum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    candidates.sort((a, b) => a - b);
+
+  var length = candidates.length;
+  var res = [];
+  search(0, [], target);
+  return res;
+
+  function search(idx, prefix, target) {
+    if (target === 0) res.push(prefix.slice());
+    if (idx === length) return;
+    if (target <= 0) return;
+
+    prefix.push(candidates[idx]);
+    search(idx, prefix, target - candidates[idx]);
+    prefix.pop();
+    search(idx + 1, prefix, target);
+  }
+};
+
+// console.log(combinationSum([2,3,6,7], 7));
+console.log(combinationSum([2,3,5], 8));
