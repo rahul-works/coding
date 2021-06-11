719d1784753ac0d546f2b5fc1b9e459841a430e3
Rahul Thakur <rahul.thakur9985@gmail.com>
2021-06-11 18:53:17 +0530

    node chat app

---
+++ project/node-group-chat/cypress/integration/first-spec.js
@@ -0,0 +1,9 @@
+it('show the message', () => {
+  cy.visit('/', {
+    onBeforeLoad(win) {
+      cy.stub(win, 'prompt').returns('Cy');
+    }
+  })
+  cy.get('#txt').type('Hello there{enter');
+  cy.contains('messages li', 'Hello there').contains('strong', 'Cy');
+});
