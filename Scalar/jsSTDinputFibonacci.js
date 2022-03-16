6c8556d84dcb1747c220a3ca26e85a48ec2b9a3f
Rahul Thakur <rahul.thakur9985@gmail.com>
2022-03-16 08:34:06 +0530

    bottom up DP

--- Scalar/jsSTDinputFibonacci.js
+++ Scalar/jsSTDinputFibonacci.js
@@ -11,19 +11,28 @@ var rl = readline.createInterface({
 
 
 rl.question("", function(N) {
-    function fibHelper(N, F) {
-        if (N === 0 || N === 1) {
-            F[N] = 1;
-            return 1;
-        }
-        if (F[N] !== undefined) {
-            return F[N];
-        }
-        F[N] = fibHelper(N-1, F)+fibHelper(N-2, F);
-        // console.log(F);
-        return F[N];
-    }
+    // function fibHelper(N, F) {
+    //     if (N === 0 || N === 1) {
+    //         F[N] = 1;
+    //         return 1;
+    //     }
+    //     if (F[N] !== undefined) {
+    //         return F[N];
+    //     }
+    //     F[N] = fibHelper(N-1, F)+fibHelper(N-2, F);
+    //     // console.log(F);
+    //     return F[N];
+    // }
     
-    console.log(fibHelper(N-1, []));
+    // bootom up approach to save space 
+    let f0=1;
+    let f1=1;
+    let f2=0;
+    for (let i = 2; i < N; i++) {
+        f2 = f0+f1;
+        f0 = f1;
+        f1 = f2;
+    }
+    console.log(f2);
    
 });
